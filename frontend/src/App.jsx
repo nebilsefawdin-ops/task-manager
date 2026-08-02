8592822625f8d3c869704c9855b09e64e8882bc5
Nebil sefawdin <nebilsefawdin@gmail.com>
2026-08-02 22:21:10 +0300

    fix: correct import path for Home component in App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import { Routes, Route } from "react-router-dom";
 import Landing from "./pages/Landing";
-import Home from "./pages/Home";
+import Home from "./pages/home";
 
 function App() {
   return (
